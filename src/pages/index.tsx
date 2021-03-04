a5009fa69a4c015faabaa45ff89eee239af8813a
flaviosantos_ <flavio@og2.com.br>
2021-03-04 20:19:20 -0300

    :wrench: Adicionado metodo handleSubmit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import { NextPage } from "next";
+import { FormEvent, useCallback, useState } from "react";
 import { FiArrowRight } from "react-icons/fi";
 
 import styles from "../styles/pages/SignIn.module.css";
 
 export const SignIn: NextPage = () => {
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      alert("teste " + username);
+    },
+    [username]
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles.backgroundContainer} />
@@ -14,11 +25,13 @@ export const SignIn: NextPage = () => {
           <img src="icons/github.svg" alt="github" />
           <p>Faça login com seu Github para começar</p>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             name="username"
             placeholder="Digite seu username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <button type="submit">
             <FiArrowRight size={24} />
